b64a296cb92c0fdd9acc9255b6f2f268e5317dbd
Taras21071988 <tsasojlov@icloud.com>
2023-09-14 12:41:14 +0300

    ...

--- src/pages/MoviePage/MoviePage.tsx
+++ src/pages/MoviePage/MoviePage.tsx
@@ -4,7 +4,7 @@ import style from './MoviePage.module.scss'
 export const MoviePage=()=>{
     return(
         <div className={style.moviePage}>
-            <Title>yi</Title>
+            <Title>Hi</Title>
         </div>
     )
 }
