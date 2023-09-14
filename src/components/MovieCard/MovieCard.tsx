89f1f2a94d13fcc12f86ca15991132b9822b7af9
Taras21071988 <tsasojlov@icloud.com>
2023-09-14 10:43:24 +0300

    creating logic for displaying an array of cards,  layout of the cards themselves

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,8 +1,11 @@
 import style from "./MovieCard.module.scss";
 
 interface MovieCardProps {
-  img?: string;
-  title?: string;
+  data: {
+    img: string;
+    title: string;
+    genre: string;
+  };
 }
 
 export const MovieCard = (props: MovieCardProps) => {
@@ -10,14 +13,12 @@ export const MovieCard = (props: MovieCardProps) => {
     <div className={style.movieCard}>
       <div className={style.imgBlock}>
         <img
-          src="https://planetakino.ua/res/get-poster/00000000000000000000000000003830/640/944/blbtl-vend2.jpg"
-          alt="Постер фильма"
+          src={props.data.img}
+          alt={"Постер фильма" + " " + props.data.title}
         />
       </div>
-      <h3 className={style.title}>Синий жук</h3>
-      <div className={style.genre}>
-        Комедия
-      </div>
+      <h3 className={style.title}>{props.data.title}</h3>
+      <div className={style.genre}>{props.data.genre}</div>
     </div>
   );
 };
---
+++ src/types/movie.ts
@@ -0,0 +1,6 @@
+export interface IMovieCard {
+  id: number;
+  img: string;
+  title: string;
+  genre: string;
+}
