cdf1b5485bd3aea1f881d08580e43cd0e9519d06
Taras21071988 <tsasojlov@icloud.com>
2023-09-16 11:51:48 +0300

    slight change in file structure

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,18 @@
 import { Link } from "react-router-dom";
 import style from "./Header.module.scss";
 import { Title } from "../Title";
+import classNames from "classnames";
 
 interface HeaderProps {
   title: string;
 }
 
-export const Header = ({title}: HeaderProps) => {
-
+export const Header = ({ title }: HeaderProps) => {
   return (
     <div className={style.header}>
-      <Link to='/' className={style.link}>Вернутся на главную</Link>
+      <Link to="/" className={classNames("hover", style.backBtn)}>
+        Вернутся на главную
+      </Link>
       <Title center>{title}</Title>
     </div>
   );
