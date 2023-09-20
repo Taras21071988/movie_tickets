5c9f3fed07c839d4d119941b9a283913d24f9b44
Taras21071988 <tsasojlov@icloud.com>
2023-09-20 11:10:28 +0300

    json-server connection

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -2,10 +2,13 @@ import { useSelector, useDispatch } from "react-redux";
 import { MovieList } from "../../components/MovieList";
 import { Title } from "../../components/Title";
 import style from "./MainPage.module.scss";
-
-import { RootState } from "../../store/store";
+import { useGetPokemonByNameQuery } from "../../api/rtkApi";
 
 export const MainPage = () => {
+  const { data, error, isLoading } = useGetPokemonByNameQuery("bulbasaur");
+
+  if (isLoading) return <h1>Loading....</h1>;
+
   return (
     <div className={style.MainPage}>
       <Title center>Выберите фильм</Title>
