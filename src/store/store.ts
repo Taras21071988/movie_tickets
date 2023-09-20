43e9b3ee471a5824abba884d44d94d74fe8d9470
Taras21071988 <tsasojlov@icloud.com>
2023-09-20 10:18:58 +0300

    test connection RTK Query

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { moviesReducer } from "../slices";
+import { pokemonApi } from "../api/rtkApi";
 
 export const store = configureStore({
   reducer: {
     movies: moviesReducer,
+    [pokemonApi.reducerPath]: pokemonApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(pokemonApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
