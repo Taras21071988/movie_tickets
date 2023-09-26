d4c9c580b7e804c063f366837ca006f53c5f8f7f
Taras21071988 <tsasojlov@icloud.com>
2023-09-26 11:26:04 +0300

    creation and layout of a grid for displaying seats in the hall

--- src/components/SeatsSelect/SeatsSelect.tsx
+++ src/components/SeatsSelect/SeatsSelect.tsx
@@ -1,13 +1,31 @@
-import style from './SeatsSelect.module.scss'
+import style from "./SeatsSelect.module.scss";
 
-export const SeatsSelect=()=>{
-    return(
-        <div className={style.seatsSelect}>
-            <div className={style.display}>
-                <i className="ic-display"></i>
-                <span>Экран</span>
-            </div>
-            <i className="ic-seat"></i>
+export const SeatsSelect = () => {
+  return (
+    <div className={style.seatsSelect}>
+      <div className={style.display}>
+        <i className="ic-display"></i>
+        <span>Экран</span>
+      </div>
+
+      <div className={style.place}>
+        <div className={style.rows}>
+          {Array(7)
+            .fill(0)
+            .map((item, i) => (
+              <div key={`${i}-${Date.now()}`} className={style.row}>
+                {i + 1}
+              </div>
+            ))}
         </div>
-    )
-}
+        <div className={style.seats}>
+          {Array(63)
+            .fill(0)
+            .map((item, i) => (
+              <i key={`${i}-${Date.now()}`} className="ic-seat" />
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
