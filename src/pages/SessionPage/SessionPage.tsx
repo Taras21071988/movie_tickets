e4e89434b130d6c2d90eab6082b55f2e958bf29a
Taras21071988 <tsasojlov@icloud.com>
2023-10-18 13:17:39 +0300

    generation of qrCode indicating session time and..  occupied seats

--- src/pages/SessionPage/SessionPage.tsx
+++ src/pages/SessionPage/SessionPage.tsx
@@ -38,7 +38,7 @@ export const SessionPage = () => {
   useEffect(() => {
     if (isSuccess) {
       setQrCode(
-        "https://api.qrserver.com/v1/create-qr-code/?size=200*200&data=tonysnow.ru "
+        `https://api.qrserver.com/v1/create-qr-code/?size=200*200&data=${sessionData?.time}${order} `
       );
     }
   }, [isSuccess]);
